91e301b6fe4d3fe5d4cc75c7f9178403c1077647
Lucas Pilette <pilettelucas@gmail.com>
2023-07-24 15:53:30 +0200

    initialisation

--- controllers/page-controller-home.php
+++ controllers/page-controller-home.php
@@ -1,2 +1,8 @@
 <?php 
 
+include(dirname(__FILE__) . '/../views/templates/header.php');
+
+include(dirname(__FILE__) . '/../views/home.php');
+
+include(dirname(__FILE__) . '/../views/templates/footer.php');
+
